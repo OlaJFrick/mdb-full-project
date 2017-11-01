6c0c97fa47deb8b8bd954a4172689b93f359b085
Ola Frick <info@culturehero.org>
2017-11-01 09:54:12 +0100

    Added CORS to app.js to allow access from the font end localhost:4200 + Changed back the backend to localhost:3000

--- app.js
+++ app.js
@@ -11,10 +11,18 @@ process.on('unhandledRejection', error=>console.log('unhandledRejection', error)
 
 const cookieSession = new Cookiesession();
 
+// Added CORS to read further: https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /* MIDDLEWARE */
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(new Cookiesession().middleware());
+
 
 /* NOT MIDDLEWARE */
 new Login(app);
@@ -42,4 +50,5 @@ global.dbQuery = Rest.query;
 
 app.use(express.static('./src'));
 
-app.listen('4200', () => console.log('MDB ACTIVE ON PORT 4200!'));
+
+app.listen('3000', () => console.log('MDB ACTIVE ON PORT 3000!'));
