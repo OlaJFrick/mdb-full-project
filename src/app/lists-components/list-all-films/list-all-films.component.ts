624a6bc831f66f6f9655c184db03cda2fea35a71
botanki <karzan@botani.nu>
2017-11-02 11:33:25 +0100

    The view ALL MOVIES now outputs data from db-view all_films_list

--- src/app/lists-components/list-all-films/list-all-films.component.ts
+++ src/app/lists-components/list-all-films/list-all-films.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../services/rest.service';
 
 @Component({
   selector: 'app-list-all-films',
   templateUrl: './list-all-films.component.html',
-  styleUrls: ['./list-all-films.component.scss']
+  styleUrls: ['./list-all-films.component.scss'],
+  providers: [
+    RestService
+  ]
 })
-export class ListAllFilmsComponent implements OnInit {
 
-  constructor() { }
+export class ListAllFilmsComponent implements OnInit {
+  mySqlData: any;
+
+  constructor(private restservice: RestService) { }
 
   ngOnInit() {
+    this.restservice.getView('all_films_list').subscribe(data => {
+      this.onDbLoad(data);
+    }, err => {
+        console.log('Error occured.');
+    });
+  }
+
+  onDbLoad(data) {
+    this.mySqlData = data.json();
+    // console.log(this.mySqlData, 'this.mySqlData');
   }
 
 }
