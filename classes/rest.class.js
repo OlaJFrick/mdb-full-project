99a25b89a26e944b4360ea66b4b28a81f0f96dd4
botanki <karzan@botani.nu>
2017-11-02 10:02:36 +0100

    Moved the SEARCH functionality from the REST class to its own class, SEARCH

--- classes/rest.class.js
+++ classes/rest.class.js
@@ -125,7 +125,7 @@ module.exports = class Rest {
     return ok;
   }
 
-  // AUTOMATICALLY RETURNS THE NEWEST VERSION OF EVERYTHING
+  /* AUTOMATICALLY RETURNS THE NEWEST VERSION OF EVERYTHING */
   selectVidify() {
 
     let table = this.table;
@@ -368,55 +368,6 @@ module.exports = class Rest {
     this.res.json(result);
   }
 
-  /* SEARCH FUNCTION */
-  async search() {
-    /*
-      från current_films vill vi ha id, title, year, genre
-      från current_actors vill vi ha id, firstName, lastName
-      från current_directors vill vi ha id, firstName, lastName
-    */
-
-    /* SEARCH THROUGH CURRENT_FILMS */
-    let filmsKeyword = 'igh';
-
-    let sqlCurrentFilms = 'SELECT id, title, year, genre FROM current_films WHERE ';
-    sqlCurrentFilms += 'title LIKE "%' + filmsKeyword + '%"';
-    sqlCurrentFilms += 'OR year LIKE "%' + filmsKeyword + '%"';
-    sqlCurrentFilms += 'OR genre LIKE "%' + filmsKeyword + '%"';
-
-    let filmsResult = await this.query(sqlCurrentFilms);
-
-    /* SEARCH THROUGH CURRENT_ACTORS */
-    let actorsKeyword = 'ska';
-
-    let sqlCurrentActors = 'SELECT id, firstName, lastName FROM current_actors WHERE ';
-    sqlCurrentActors += 'firstName LIKE "%' + actorsKeyword + '%"';
-    sqlCurrentActors += 'lastName LIKE "%' + actorsKeyword + '%"';
-
-    let actorsResult = await this.query(sqlCurrentActors);
-
-    /* SEARCH THROUGH CURRENT_DIRECTORS */
-    let directorsKeyword = 'ska';
-
-    let sqlCurrentDirectors = 'SELECT id, firstName, lastName FROM current_directors WHERE ';
-    sqlCurrentDirectors += 'firstName LIKE "%' + directorsKeyword + '%"';
-    sqlCurrentDirectors += 'lastName LIKE "%' + directorsKeyword + '%"';
-
-    let DirectorsResult = await this.query(sqlCurrentDirectors);
-
-    // console.log({
-    //   films: filmsResult,
-    //   actors: actorsResult,
-    //   directors: directorsResult
-    // });
-
-    this.res.json({
-      films: filmsResult,
-      actors: actorsResult,
-      directors: directors
-    });
-  }
-
   async findUserToBan() {
     // CHECK IF THERE ARE USERS TO BAN & DELETE
     let checkUsers = await this.query('SELECT * FROM users WHERE warnings = 3 && role != "banned"');
@@ -429,7 +380,7 @@ module.exports = class Rest {
 
     this.next();
     return;
-  };
+  }
 
   async deleteUserPosts(foundUser) {
     let tables = ['films', 'persons', 'reviews', 'films_actors', 'films_directors'];
