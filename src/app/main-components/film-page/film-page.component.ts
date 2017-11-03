eedbd5c82342cf0b15e530e5bd27dd118fa02c46
Ola Frick <info@culturehero.org>
2017-11-03 12:57:13 +0100

    Added 'reviews' & a form for adding new review to 'film-page'

--- src/app/main-components/film-page/film-page.component.ts
+++ src/app/main-components/film-page/film-page.component.ts
@@ -13,28 +13,49 @@ import { StarRatingComponent } from '../../reusable-components/star-rating/star-
   ]
 })
 export class FilmPageComponent implements OnInit {
-  filmdata: any = {};
+  film: any = {};
+  reviews: any;
   filmid: any;
-  film: any;
+  reviewstatus = '';
+  reviewForm = false;
+  // film: any;
 
   constructor(private restservice: RestService, private location: Location) {
-    this.filmdata.imagePath = 'default.png';
+    this.film.imagePath = 'default.png';
   }
 
   ngOnInit() {
-    this.filmid = this.location.path().slice(11);
+    this.filmid = Number (this.location.path().slice(11));
 
     this.restservice.get('all_films_list', this.filmid).then(data => {
-      this.filmdata = data.json();
+      this.film = data.json();
     }, err => {
       console.log('Error occured.');
     });
 
+    this.restservice.get('all_reviews_list').then(data => {
+        this.loadReviews(data);
+    }, err => {
+        console.log('Error occured.');
+    });
   }
+
+  loadReviews(data) {
+    this.reviews = data.json().filter((res) => {
+      return res.id === this.filmid;
+    });
+
+    if (this.reviews.length === 0) {
+      this.reviewstatus = 'No reviews';
+    }
+  }
+
+  onSubmit(form: any): void {
+      console.log('you submitted value:', form);
+    }
 
   saveEditable(value) {
     // call to http service
     console.log('http.service: ' + value);
   }
-
 }
