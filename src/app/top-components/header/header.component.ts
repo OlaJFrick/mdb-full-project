4d0c7e73205d09f343969eb6ea1e946cf3ec90e7
Bjorn Ohlsson <bjorn.ohlsson@glimworks.com>
2017-11-06 10:59:21 +0100

    Updated version of header

--- src/app/top-components/header/header.component.ts
+++ src/app/top-components/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../services/rest.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [
+    RestService
+  ]
 })
 export class HeaderComponent implements OnInit {
+	mySqlData: any;
+	isNavbarCollapsed = true;
 
-  constructor() { }
+  constructor(/*private restservice: RestService*/) { }
 
   ngOnInit() {
+  	/*this.restservice.get('').then(data => {
+      this.onDbLoad(data);
+    }, err => {
+        console.log('Error occured.');
+    });*/
   }
+
+  /*onDbLoad(data) {
+    this.mySqlData = data.json();
+     console.log(this.mySqlData, 'this.mySqlData');
+  }*/
 
 }
