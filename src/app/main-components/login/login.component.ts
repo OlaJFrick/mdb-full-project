ae9dbc82b3e9ef9e968bc2e63ce5520d884e3d7d
botanki <karzan@botani.nu>
2017-11-06 13:56:00 +0100

    Fixed bug on Person Page where you could only see one film a person has acted in and/or directed. And also added login functionality.

--- src/app/main-components/login/login.component.ts
+++ src/app/main-components/login/login.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Router } from '@angular/router';
+import { RestService } from '../../services/rest.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [
+    RestService,
+    Location, { provide: LocationStrategy, useClass: PathLocationStrategy }
+  ]
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent {
 
-  ngOnInit() {
+  credentials = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private restservice: RestService, private router: Router) {
+  }
+
+  login() {
+    this.restservice.post('login', this.credentials).then(res => {
+      res.json();
+
+      if (res.json().status === 'logged in') {
+        this.router.navigateByUrl('/');
+      }
+
+      console.log('you\'re in:', res.json());
+    }, err => {
+      console.log('you\'re out:');
+    });
   }
 
 }
