5d077aa7a1fe147463d468e1af53f3e9143f5ab2
Bjorn Ohlsson <bjorn.ohlsson.93@gmail.com>
2017-11-07 11:16:21 +0100

    Updated version of header and sidebar. A temporary start/close button

--- src/app/top-components/header/header.component.ts
+++ src/app/top-components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
 import { RestService } from '../../services/rest.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+   animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(-100%, 0, 0)'
+      })),
+      transition('in => out', animate('300ms ease-in-out')),
+      transition('out => in', animate('300ms ease-in-out'))
+    ]),
+  ],
   providers: [
     RestService
   ]
@@ -12,6 +24,8 @@ import { RestService } from '../../services/rest.service';
 export class HeaderComponent implements OnInit {
 	mySqlData: any;
 	isNavbarCollapsed = true;
+
+	menuState:string = 'out';
 
   constructor(/*private restservice: RestService*/) { }
 
@@ -23,9 +37,16 @@ export class HeaderComponent implements OnInit {
     });*/
   }
 
+  toggleMenu() {
+    // 1-line if statement that toggles the value:
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  }
+
   /*onDbLoad(data) {
     this.mySqlData = data.json();
      console.log(this.mySqlData, 'this.mySqlData');
   }*/
 
 }
+
+
