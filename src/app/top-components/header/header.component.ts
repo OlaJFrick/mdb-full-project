dbbb2ba928de20a04364bd68718ad46ea6763bac
botanki <karzan@botani.nu>
2017-11-07 11:35:49 +0100

    You can now LOG OUT from the website.

--- src/app/top-components/header/header.component.ts
+++ src/app/top-components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../services/rest.service';
+import { GlobalService } from '../../services/global.service';
+import { Router } from '@angular/router';
+import { LoginComponent } from '../../main-components/login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -12,8 +15,11 @@ import { RestService } from '../../services/rest.service';
 export class HeaderComponent implements OnInit {
 	mySqlData: any;
 	isNavbarCollapsed = true;
+  header: LoginComponent;
 
-  constructor(/*private restservice: RestService*/) { }
+  constructor(private restservice: RestService, private globalservice: GlobalService, private router: Router) {
+    this.header = new LoginComponent(restservice, router, globalservice);
+  }
 
   ngOnInit() {
   	/*this.restservice.get('').then(data => {
